2fa307879430d179437593738841da87f1dda795
eivindw <eivindw@gmail.com>
2016-03-28 22:10:57 +0200

    Use latest version of disruptor - and Java 8.

--- src/main/java/com/trevorbernard/disruptor/examples/Simple.java
+++ src/main/java/com/trevorbernard/disruptor/examples/Simple.java
@@ -1,12 +1,11 @@
 package com.trevorbernard.disruptor.examples;
 
+import com.lmax.disruptor.RingBuffer;
+import com.lmax.disruptor.dsl.Disruptor;
+
 import java.util.UUID;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-
-import com.lmax.disruptor.EventHandler;
-import com.lmax.disruptor.RingBuffer;
-import com.lmax.disruptor.dsl.Disruptor;
 
 public class Simple {
     @SuppressWarnings("unchecked")
@@ -14,15 +13,14 @@ public class Simple {
         ExecutorService exec = Executors.newCachedThreadPool();
         // Preallocate RingBuffer with 1024 ValueEvents
         Disruptor<ValueEvent> disruptor = new Disruptor<ValueEvent>(ValueEvent.EVENT_FACTORY, 1024, exec);
-        final EventHandler<ValueEvent> handler = new EventHandler<ValueEvent>() {
-            // event will eventually be recycled by the Disruptor after it wraps
-            public void onEvent(final ValueEvent event, final long sequence, final boolean endOfBatch) throws Exception {
-                System.out.println("Sequence: " + sequence);
-                System.out.println("ValueEvent: " + event.getValue());
-            }
-        };
         // Build dependency graph
-        disruptor.handleEventsWith(handler);
+        disruptor.handleEventsWith((event, sequence, endOfBatch) -> {
+            System.out.printf(
+                    "Sequence: %d Thread: %s%n",
+                    sequence, Thread.currentThread().getName()
+            );
+            System.out.println("ValueEvent: " + event.getValue());
+        });
         RingBuffer<ValueEvent> ringBuffer = disruptor.start();
 
         for (long i = 10; i < 2000; i++) {
